2d310502eca3bafd86ab57c3bd5e0d2e4ea0e3e3
Brian Crosby <bcrosby@2checkout.com>
2015-10-22 14:41:04 -0400

    Added subscriptions table to welcome

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,5 +3,6 @@ class StaticPagesController < ApplicationController
   def welcome
     #TODO: fill out things in the /views/static_pages/welcome
     @title = " - Home"
+    @plans = Stripe::Plan.all().sort { |a,b| a.name <=> b.name }
   end
 end
