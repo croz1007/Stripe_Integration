11a15bd886625028f9aef3fead54e81b0d9429fa
Brian Crosby <bcrosby@2checkout.com>
2015-10-22 10:34:10 -0400

    Added method to create Stripe customer when new app customer created

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,19 @@
+require 'pry'
+
 class Customer < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  before_create :register_stripe_customer
+
+  def register_stripe_customer
+    email = self.email
+
+    Stripe::Customer.create(
+      :email => email
+    )
+  end
+
 end
