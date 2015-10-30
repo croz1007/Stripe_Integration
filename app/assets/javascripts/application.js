3c3562a90e20189f94564bbc7492450af59dd3d6
Brian Crosby <bcrosby@2checkout.com>
2015-10-30 15:22:59 -0400

    Added ability to add new cards to customer

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,48 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+// Used temporarily for Navigation background
+$(document).ready(function() {
+  var menuToggle = $('#js-mobile-menu').unbind();
+  $('#js-navigation-menu').removeClass("show");
+
+  menuToggle.on('click', function(e) {
+    e.preventDefault();
+    $('#js-navigation-menu').slideToggle(function(){
+      if($('#js-navigation-menu').is(':hidden')) {
+        $('#js-navigation-menu').removeAttr('style');
+      }
+    });
+  });
+
+  $("#btn_new_card").on("click", function(){
+    createStripeToken();
+  });
+});
+
+function createStripeToken(){
+  Stripe.setPublishableKey('pk_test_8Watr62QvzgQHnLtBbf1yR0Q');
+  Stripe.card.createToken({
+    number: $("#number").val(),
+    cvc: $("#cvc").val(),
+    exp_month: $("#exp_month").val(),
+    exp_year: $("#exp_year").val(),
+    name: $("#name").val(),
+    address_line1: $("#address_line1").val(),
+    address_line2: $("#address_line2").val(),
+    address_city: $("#address_city").val(),
+    address_state: $("#address_state").val(),
+    address_zip: $("#address_zip").val(),
+    address_country: $("#address_country").val()
+  }, stripeResponseHandler);
+}
+
+function stripeResponseHandler(status, response){
+  $("#token").val(response.id);
+  formSubmit();
+}
+
+function formSubmit(){
+  $("#form_new_card").submit();
+}
--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,4 +1,4 @@
-# require 'pry'
+require 'pry'
 
 class CardsController < ApplicationController
   before_filter :get_stripe_customer
@@ -9,7 +9,7 @@ class CardsController < ApplicationController
 
   def destroy
     @customer.sources.retrieve(params[:id]).delete
-    redirect_to cards_url, :notice => "Card Deleted"
+    sendToProfile("Card Deleted")
   end
 
   def edit
@@ -19,46 +19,41 @@ class CardsController < ApplicationController
 
   def update
     @card = @customer.sources.retrieve(params[:id])
-
-    if !params[:address_city].blank?
-      @card.address_city = params[:address_city]
-    end
-    if !params[:address_country].blank?
-      @card.address_country = params[:address_country]
-    end
-    if params[:address_line1].blank?
-      @card.address_line1 = params[:address_line1]
-    end
+    @card.address_city = params[:address_city] unless params[:address_city].blank?
+    @card.address_country = params[:address_country] unless params[:address_country].blank?
+    @card.address_line1 = params[:address_line1] unless params[:address_line1].blank?
     if !params[:address_line2].blank?
       @card.address_line2 = params[:address_line2]
+    else
+      @card.address_line2 = " "
     end
-    if !params[:address_state].blank?
-      @card.address_state = params[:address_state]
-    end
-    if !params[:address_zip].blank?
-      @card.address_zip = params[:address_zip]
-    end
-    if !params[:exp_month].blank?
-      @card.exp_month = params[:exp_month]
-    end
-    if !params[:exp_year].blank?
-      @card.exp_year = params[:exp_year]
-    end
-    if !params[:name].blank?
-      @card.name = params[:name]
-    end
+    @card.address_state = params[:address_state] unless params[:address_state].blank?
+    @card.address_zip = params[:address_zip] unless params[:address_zip].blank?
+    @card.exp_month = params[:exp_month]unless params[:exp_month].blank?
+    @card.exp_year = params[:exp_year] unless params[:exp_year].blank?
+    @card.name = params[:name] unless params[:name].blank?
     @card.save
-    redirect_to profiles_url, :notice => "Card Updated"
+    sendToProfile("Card Updated")
   end
 
   def new
-    
+
+  end
+
+  def create
+    @token = params[:token]
+    @customer.sources.create(:source => @token)
+    sendToProfile("New Card Added")
   end
 
   protected
 
   def get_stripe_customer
     @customer ||= Stripe::Customer.retrieve(current_customer.stripe_id)
+  end
+
+  def sendToProfile (message)
+    redirect_to profiles_url,  :notice => message
   end
 
 end
