128aa0029db659fdeb8b37e84e76359b7a0f4f7d
Brian Crosby <bcrosby@2checkout.com>
2015-11-05 11:07:21 -0500

    Setting up Webhooks

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -3,7 +3,7 @@ require 'json'
 class WebhooksController < ApplicationController
 
   def index
-    @response = JSON.parse({"response": params[:event_json]})
+    @response = JSON.parse(params[:event_json])
   end
 
 end
